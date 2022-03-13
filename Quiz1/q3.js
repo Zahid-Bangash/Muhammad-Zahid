21c9e1d5d1ca214af66daf666ec58146f1af5346
Muhammad Zahid <101349002+Zahid-Bangash@users.noreply.github.com>
2022-03-13 23:22:17 +0500

    Delete q3.js

--- Quiz1/q3.js
+++
@@ -1,4 +0,0 @@
-let myArray=['a','b','c','d'];
-myArray.unshift('start');  //added element to the start of the array
-myArray.push('end');        //added element to the end of the array
-console.log(myArray);
