dd59dad0df3c9cda698ecd480ea51c1b6deb6713
Muhammad Zahid <101349002+Zahid-Bangash@users.noreply.github.com>
2023-04-02 14:25:28 +0500

    match...

--- my-app/app/screens/MatchCenter.js
+++ my-app/app/screens/MatchCenter.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
 import Swiper from "react-native-swiper";
+import Entypo from "@expo/vector-icons/Entypo";
 
 export default function MatchCenter() {
   const [swiperIndex, setSwiperIndex] = useState(0);
@@ -9,14 +16,7 @@ export default function MatchCenter() {
     setSwiperIndex(index);
   };
   const renderPagination = () => (
-    <View
-      style={{
-        flexDirection: "row",
-        position: "absolute",
-        borderBottomWidth: 1,
-        borderColor: "gray",
-      }}
-    >
+    <View style={styles.pagination}>
       <TouchableOpacity
         onPress={() => handleButtonPress(1)}
         style={[styles.button, swiperIndex === 0 && styles.activeButton]}
@@ -52,7 +52,7 @@ export default function MatchCenter() {
             marginTop: 44,
             justifyContent: "center",
             alignItems: "center",
-            borderBottomWidth: 1,
+            borderBottomWidth: 0.5,
             borderColor: "gray",
           }}
         >
@@ -63,9 +63,8 @@ export default function MatchCenter() {
         <View
           style={{
             flex: 2.5,
-            borderBottomWidth: 1,
+            borderBottomWidth: 0.5,
             borderColor: "gray",
-            alignItems: "center",
             width: "100%",
             flexDirection: "row",
             padding: 5,
@@ -73,81 +72,44 @@ export default function MatchCenter() {
         >
           <View
             style={{
-              flex: 2.5,
+              flex: 2,
               justifyContent: "space-around",
-              height: "100%",
-              alignItems: "center",
             }}
           >
-            <Text style={{ fontWeight: "bold" }}>Batsman</Text>
-            <Text>Zahid*</Text>
-            <Text>Bangash</Text>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>Batsman</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>Zahid*</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>Bangash</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-              height: "100%",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>R</Text>
-            <Text>54</Text>
-            <Text>54</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>R</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-              height: "100%",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>B</Text>
-            <Text>54</Text>
-            <Text>54</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>B</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-              height: "100%",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>4s</Text>
-            <Text>5</Text>
-            <Text>54</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>4s</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>5</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-              height: "100%",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>6s</Text>
-            <Text>1</Text>
-            <Text>54</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>6s</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>1</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-              height: "100%",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>SR</Text>
-            <Text>254</Text>
-            <Text>54</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>SR</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>254</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
           </View>
         </View>
         <View
           style={{
             flex: 2,
-            borderBottomWidth: 1,
+            borderBottomWidth: 0.5,
             borderColor: "gray",
             width: "100%",
             flexDirection: "row",
@@ -155,71 +117,40 @@ export default function MatchCenter() {
         >
           <View
             style={{
-              flex: 2.5,
+              flex: 2,
               justifyContent: "space-around",
               height: "100%",
-              alignItems: "center",
               padding: 5,
             }}
           >
-            <Text style={{ fontWeight: "bold" }}>Bowler</Text>
-            <Text>Zahid</Text>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>Bowler</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>Zahid</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>O</Text>
-            <Text>54</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>O</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>M</Text>
-            <Text>54</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>M</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>54</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>R</Text>
-            <Text>5</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>R</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>5</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>W</Text>
-            <Text>1</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>W</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>1</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "space-around",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontWeight: "bold" }}>Eco</Text>
-            <Text>254</Text>
+          <View style={styles.cell}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>Eco</Text>
+            <Text style={{ fontWeight: "bold", color: "grey" }}>254</Text>
           </View>
         </View>
         <View
           style={{
             flex: 1.5,
-            borderBottomWidth: 1,
+            borderBottomWidth: 0.5,
             borderColor: "gray",
             width: "100%",
           }}
@@ -232,7 +163,109 @@ export default function MatchCenter() {
             width: "100%",
           }}
         ></View>
-        <View style={{ flex: 4, backgroundColor: "red", width: "100%" }}></View>
+        <View
+          style={{
+            flex: 4,
+            backgroundColor: "pink",
+            width: "100%",
+            padding: 5,
+          }}
+        >
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <TouchableWithoutFeedback onPress={() => console.log("dot")}>
+              <View style={styles.buttonCell}>
+              <Entypo name="dot-single" size={30} />
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("1")}>
+              <View style={styles.buttonCell}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>1</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("2")}>
+              <View style={styles.buttonCell}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>2</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("3")}>
+              <View style={styles.buttonCell}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>3</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("undo")}>
+              <View style={[styles.buttonCell, { borderRightWidth: 0 }]}>
+                <Text
+                  style={{ fontWeight: "bold", fontSize: 17, color: "green" }}
+                >
+                  Undo
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <TouchableWithoutFeedback onPress={() => console.log("wide")}>
+              <View style={styles.buttonCell}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>Wide</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("Bye")}>
+              <View style={styles.buttonCell}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>Bye</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("4")}>
+              <View style={styles.buttonCell}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>4</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("6")}>
+              <View style={styles.buttonCell}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>6</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("out")}>
+              <View style={[styles.buttonCell, { borderRightWidth: 0 }]}>
+                <Text
+                  style={{ fontWeight: "bold", fontSize: 17, color: "red" }}
+                >
+                  Out
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <TouchableWithoutFeedback onPress={() => console.log("nb")}>
+              <View style={[styles.buttonCell, { borderBottomWidth: 0 }]}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>NB</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("lb")}>
+              <View style={[styles.buttonCell, { borderBottomWidth: 0 }]}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>LB</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("5,7")}>
+              <View style={[styles.buttonCell, { borderBottomWidth: 0 }]}>
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>5,7</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("input")}>
+              <View style={[styles.buttonCell, { borderBottomWidth: 0 }]}>
+                <Entypo name="pencil" size={25} />
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => console.log("more")}>
+              <View
+                style={[
+                  styles.buttonCell,
+                  { borderRightWidth: 0, borderBottomWidth: 0 },
+                ]}
+              >
+                <Text style={{ fontWeight: "bold", fontSize: 17 }}>More</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </View>
       </View>
       <View style={styles.slide}>
         <Text style={styles.buttonText}>Scorecard Screen</Text>
@@ -255,6 +288,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  pagination: {
+    flexDirection: "row",
+    position: "absolute",
+    borderBottomWidth: 1,
+    borderColor: "gray",
+  },
   buttonText: {
     fontSize: 16,
   },
@@ -266,5 +305,18 @@ const styles = StyleSheet.create({
   activeButton: {
     borderBottomWidth: 3,
     borderColor: "black",
+  },
+  cell: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  buttonCell: {
+    flex: 1,
+    borderColor: "black",
+    borderBottomWidth: 0.5,
+    borderRightWidth: 0.5,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
