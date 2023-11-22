7c44bb367c5d96f90f54e1c991d6df6366603b11
Muhammad Zahid <101349002+Zahid-Bangash@users.noreply.github.com>
2023-11-22 14:09:40 +0500

    ...

---
+++ nextapp/src/app/page.js
@@ -0,0 +1,16 @@
+const page = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "50vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Next js App
+    </div>
+  );
+};
+
+export default page;
